5839f0a8f5ab3f55e7609fbfb4350b0648731179
Yaroslav Yermilov <yermilov17@gmail.com>
2016-04-14 12:56:43 +0300

    yo angular generated project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hypheedWebstubApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hypheedWebstubApp
+ */
+angular.module('hypheedWebstubApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hypheedWebstubApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hypheedWebstubApp
+ */
+angular.module('hypheedWebstubApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
