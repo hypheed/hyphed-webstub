a84c83300338d1638dd3d30b7e8767b6326e8f36
Yaroslav Yermilov <yermilov17@gmail.com>
2016-04-16 16:23:00 +0300

    #1. create simple stub web page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,23 +8,25 @@
  *
  * Main module of the application.
  */
-angular
-  .module('hypheedWebstubApp', [
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+angular .module('hypheedWebstubApp', ['ui.router', 'ngResource'])
+        .config(function($stateProvider, $urlRouterProvider) {
+
+            $stateProvider
+                // route for the home page
+                .state('app', {
+                    url: '/',
+                    views: {
+                        'header': {
+                            templateUrl : 'views/header.html',
+                        },
+                        'content': {
+                            templateUrl : 'views/home.html'
+                        },
+                        'footer': {
+                            templateUrl : 'views/footer.html',
+                        }
+                    }
+                });
+
+            $urlRouterProvider.otherwise('/');
+        });
